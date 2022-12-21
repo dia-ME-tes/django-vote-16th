b003fd5eed9e9a7dec6981843ba33c81a373a13e
suabae <bsa0322@ewhain.net>
2022-12-21 20:54:35 +0900

    [FIX] cors allow

--- django_vote_16th/settings/base.py
+++ django_vote_16th/settings/base.py
@@ -86,6 +86,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
